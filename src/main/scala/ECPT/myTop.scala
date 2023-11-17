53bf49936f047009aaea359b0055f1d0103ee92a
PeterDing372 <quanchending@126.com>
2023-11-17 11:13:21 -0500

    use self clone rocket-chip

--- src/main/scala/ECPT/myTop.scala
+++ src/main/scala/ECPT/myTop.scala
@@ -4,10 +4,18 @@ import chisel3._
 import freechips.rocketchip.tile._ 
 import freechips.rocketchip.config.Parameters
 import ECPT.PTW._
+import freechips.rocketchip.rocket._
+import scala.collection.mutable.ListBuffer
 
 
 class myTop (implicit p : Parameters) extends CoreModule()(p) {
     val ptw  = Module(new BOOM_PTW(1)(p))
+    val hellaCachePorts  = ListBuffer[HellaCacheIO]()
+    hellaCachePorts += ptw.io.mem
+    val hellaCacheArb = Module(new HellaCacheArbiter(hellaCachePorts.length)(p))
+    hellaCacheArb.io.requestor <> hellaCachePorts.toSeq
+    // hellaCacheArb.io.mem <>
+
     // lazy val dcache: BoomNonBlockingDCache = LazyModule(new BoomNonBlockingDCache(staticIdForMetadataUseOnly))
     // val dcache = new BoomNonBlockingDCache(staticIdForMetadataUseOnly
 }
