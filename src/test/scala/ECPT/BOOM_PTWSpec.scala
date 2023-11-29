f878a908a3eac597886b27df8674707115aac179
PeterDing372 <quanchending@126.com>
2023-11-29 00:38:46 -0500

    fixed issue with tag not stored properly in ECPTSpec

--- src/test/scala/ECPT/BOOM_PTWSpec.scala
+++ src/test/scala/ECPT/BOOM_PTWSpec.scala
@@ -98,12 +98,44 @@ class BoomECPTSpec extends AnyFreeSpec with ChiselScalatestTester{
         dut.clock.step()
     }
 
+    def formatPTE(reserved_for_future: Long,
+                  ppn: Long,
+                  reserved_for_software: Long = 0,
+                  d: Long = 0,
+                  a: Long = 0,
+                  g: Long = 0,
+                  u: Long = 0,
+                  x: Long = 0,
+                  w: Long = 0,
+                  r: Long = 1,
+                  v: Long = 0
+                  ): UInt = {
+      // var r_pte = UInt(64.W)
+      val r_pte = ((reserved_for_future << 54) | 
+                 (ppn << 10) | 
+                 (reserved_for_software << 8) | 
+                 (d << 7) | 
+                 (a << 6) | 
+                 (g << 5) | 
+                 (u << 4) | 
+                 (x << 3) | 
+                 (w << 2) | 
+                 (r << 1) | 
+                 v)
+      println(s"[formatPTE] r_pte ${r_pte.toBinaryString}")
+      r_pte.U // & ~0.U(64.W)
+    }
+
+
 
     val s_ready :: s_hashing :: s_traverse1 :: s_traverse2 :: s_req :: s_wait1 :: s_done :: Nil 
       = Enum(7)
 
     "BoomECPTSpec should hash complete within 28 cycles" in {
         test(new BOOM_PTW(1)(para) ) { c =>
+            /* use this section to test formatPTE  */
+            // val test_result = formatPTE(0, 1)
+            // println(s"test_result ${test_result.litValue.toInt.toBinaryString}")
             cycle = 0
             println("SIMULATION [Start]: write request to Boom_PTW")
             val vpnAddr = formRadixReqAddr(1, 1, 1)
@@ -134,11 +166,13 @@ class BoomECPTSpec extends AnyFreeSpec with ChiselScalatestTester{
             debug.ptwState.expect(s_traverse1)
             stepClock(c) // one step without valid response 
             memIO.resp.valid.poke(true)
+            memIO.resp.bits.data.poke(formatPTE(0x4, 0x1)) // true response
             for (i <- 0 until 4) {
               stepClock(c) 
             }
-            // one step without valid response
+            // step without valid response
             memIO.resp.valid.poke(false)
+            memIO.resp.bits.data.poke(formatPTE(0x3, 0xF)) // false response
             stepClock(c)  
             for (i <- 0 until 10) {
               stepClock(c) 
@@ -147,37 +181,82 @@ class BoomECPTSpec extends AnyFreeSpec with ChiselScalatestTester{
             stepClock(c)
             // continue stepping to s_traverse2
             memIO.resp.valid.poke(true)
-            for (i <- 0 until 6) {
+            memIO.resp.bits.data.poke(formatPTE(0x4, 0x1)) // true response
+            for (i <- 0 until 4) {
               stepClock(c) 
             }
+            memIO.resp.valid.poke(false)
+            stepClock(c) 
             debug.ptwState.expect(s_traverse2)
+            memIO.resp.valid.poke(true)
+            memIO.resp.bits.data.poke(formatPTE(0x7, 0x2)) // true response
+            for (i <- 0 until 8) {
+              stepClock(c) 
+            }
+            memIO.resp.valid.poke(false)
+            stepClock(c) 
+            debug.ptwState.expect(s_done)
             stepClock(c)
 
-
-
-
-
-
-
             // printDebugInfo(debug)
             // printMemIO(memIO)
-            // stepClock(c)
-            // // assume cache received request and set resp is ready
-            // setCacheReqState(memIO, true.B)
-            // printDebugInfo(debug)
-            // printMemIO(memIO)
-            // stepClock(c)
-            // printDebugInfo(debug)
-            // printMemIO(memIO)
-            // debug.ptwState.expect(s_wait1)
-            // stepClock(c)
-            // printDebugInfo(debug)
-            // printMemIO(memIO)
-            
 
         }
     }
-    /* Note request does not stay on arb out if we remove the request */
+    
+
+    "BoomECPTSpec reg store fetched data" in {
+        test(new BOOM_PTW(1)(para) ) { c =>
+            /* use this section to test formatPTE  */
+            // val test_result = formatPTE(0, 1)
+            // println(s"test_result ${test_result.litValue.toInt.toBinaryString}")
+            cycle = 0
+            println("SIMULATION [Start]: write request to Boom_PTW")
+            val vpnAddr = formRadixReqAddr(1, 1, 1)
+            println(s"SIMULATION [vpnAddr]: hex ${vpnAddr.toHexString} /  ${vpnAddr.toBinaryString}")
+            val debug = c.io.debug
+            val requestor = c.io.requestor
+            val memIO = c.io.mem
+            requestor(0).req.valid.poke(false)
+            memIO.resp.valid.poke(false)
+            stepClock(c)
+            stepClock(c)
+            debug.ptwState.expect(s_ready)
+            stepClock(c)
+            writePTWReq(requestor(0).req, vpnAddr, stage2 = true.B)
+            val reqObj = requestor(0).req
+            reqObj.valid.poke(true)
+            reqObj.bits.valid.poke(true)
+            println("[TEST] start hashing")
+            for (i <- 0 until 30) {
+                println(s"hash cycle ${i}")
+                stepClock(c)
+            }
+            stepClock(c)
+            debug.ptwState.expect(s_traverse1)
+            stepClock(c) // one step without valid response 
+            for (i <- 0 until 8) {
+              memIO.resp.valid.poke(true)
+              memIO.resp.bits.data.poke(formatPTE(0x4, i)) // true response
+              stepClock(c) 
+            }
+            memIO.resp.valid.poke(false)
+            stepClock(c) 
+            debug.ptwState.expect(s_traverse2)
+            stepClock(c)  
+            stepClock(c)
+            for (i <- 0 until 8) {
+              memIO.resp.valid.poke(true)
+              memIO.resp.bits.data.poke(formatPTE(0x7, i*2)) // true response
+              stepClock(c) 
+            }
+            memIO.resp.valid.poke(false)
+            stepClock(c) 
+            debug.ptwState.expect(s_done)
+            stepClock(c)
+
+        }
+    }
     
     
 }
